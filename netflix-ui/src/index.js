f57baf82ae026c79fc5c1c6478406f1cb2198601
Levan277 <levan.seneviratne@sherdiancollege.ca>
2024-02-05 19:07:12 -0500

    production ready

--- netflix-ui/src/index.js
+++ netflix-ui/src/index.js
@@ -6,6 +6,10 @@ import App from "./App";
 import { store } from "./store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
